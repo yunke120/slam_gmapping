f570021ef66c0d8bb618b9849e230c1d26b7af6e
yunke120 <fj_18860352816@163.com>
2024-01-04 21:41:06 +0800

    update

--- slam_gmapping/launch/slam_gmapping.launch.py
+++ slam_gmapping/launch/slam_gmapping.launch.py
@@ -2,11 +2,23 @@ from launch import LaunchDescription
 from launch.substitutions import EnvironmentVariable
 import launch.actions
 import launch_ros.actions
+import os
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='true')
+    rviz_config_dir = os.path.join(get_package_share_directory('slam_gmapping'),
+                                   'rviz', 'bot_gmapping.rviz')
     return LaunchDescription([
         launch_ros.actions.Node(
-            package='slam_gmapping', node_executable='slam_gmapping', output='screen', parameters=[{'use_sim_time':use_sim_time}]),
+            package='slam_gmapping', executable='slam_gmapping', output='screen', parameters=[{'use_sim_time':use_sim_time}]),
+
+        launch_ros.actions.Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
     ])
